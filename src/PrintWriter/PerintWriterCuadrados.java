d7835d0dd01e8ad409a786f937e2d09dea601c7b
Jorgesrab <jorge.sanchez174@educa.madrid.org>
2024-10-18 14:02:16 +0200

    commit

---
+++ src/PrintWriter/PerintWriterCuadrados.java
@@ -0,0 +1,23 @@
+package PrintWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class PerintWriterCuadrados {
+    public static void main(String[] args) {
+        String nombreArchivo = "C:\\Users\\Alumno\\Desktop\\PrintWriter.txt";
+
+        try(PrintWriter pw = new PrintWriter(new FileWriter(new File(nombreArchivo),true))) {
+
+            for (int i = 1; i <= 10; i++) {
+                double cuadrado = i*i;
+                pw.printf("El cuadrado de %d es igual a %f %n",i,cuadrado);
+            }
+
+
+      } catch (Exception e) {
+          throw new RuntimeException(e);
+      }
+    }
+}
--- src/PrintWriter/PrintWriter1.java
+++ src/PrintWriter/PrintWriter1.java
@@ -3,17 +3,29 @@ package PrintWriter;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class PrintWriter1 {
     public static void main(String[] args) {
         // Nombre del archivo donde se escribirá el contenido
+        Scanner sc = new Scanner(System.in);
         String nombreArchivo = "C:\\Users\\Alumno\\Desktop\\PrintWriter.txt";
 
+
+        System.out.println("Escribe un primer numero");
+        int numero1 = sc.nextInt();
+
+        System.out.println("escribe un segundo numero");
+        int numero2 = sc.nextInt();
+
+        int suma = numero1 + numero2;
+
+
         // Texto que se va a escribir en el archivo
-        Integer contenido = 124;
+
         // Uso de FileWriter para escribir en el archivo
         try (PrintWriter writer = new PrintWriter(new FileWriter(nombreArchivo))) {
-            writer.println(contenido); // Escribir el contenido
+            writer.printf("Operacion: %d + %d = %d",numero1,numero2,suma); // Escribir el contenido
             System.out.println("Contenido escrito en el archivo: " + nombreArchivo);
         } catch (IOException e) {
             e.printStackTrace(); // Manejo de excepciones
